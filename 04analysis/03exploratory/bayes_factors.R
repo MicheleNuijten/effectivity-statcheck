aa520864919ed365b82e83e036eff1dc64cb95d3
MicheleNuijtene <michele.nuijten@gmail.com>
2023-07-18 11:27:04 +0200

    update exploratory analyses

---
+++ 04analysis/03exploratory/bayes_factors.R
@@ -0,0 +1,100 @@
+# exploratory analysis:
+# calculate bayes factors for linear models
+
+rm(list = ls())
+
+library(BFpack)
+library(lme4)
+
+# LOAD DATA --------------------------------------------------------------------
+
+data <- read.table("03data/2023-06-15data_wrangled_no_missings.txt", header = TRUE)
+data_per_article <- read.table("03data/2023-06-15data_per_article_with_stats.txt", header = TRUE)
+
+# ANALYSIS 1: Predict inconsistencies ------------------------------------------
+
+# fit multilevel logistic model
+lm_errors <- glmer(error ~ period*statcheck_journal + (1|source), 
+                   data = data, family = binomial(link = "logit"))
+
+summary(lm_errors)
+
+##################
+
+### Fit null model in order to compute the intraclass correlation
+null <- glmer(error ~ (1|source), data = data, family = binomial(link = "logit"))
+
+var_int <- as.numeric(summary(null)$varcor$source) # Variance of random intercept
+
+### Compute Bayes factor
+### Compute ICC in order to compute the effective sample size that is needed as 
+# input for the BF() function (see p. 117 of Hox, Moerbeek, and Van de Schoot, 
+# 2018 for the computation of the ICC)
+icc <- var_int/(var_int+pi^2/3)
+
+### Compute mean cluster size
+tmp <- split(data, f = data$source)
+cluster_size <- sapply(tmp, function(x) nrow(x))
+mean_cluster_size <- mean(cluster_size)
+
+### Compute effective sample size (equation 12.5 in Hox, Moerbeek and Van de 
+# Schoot, 2018, p. 223)
+design <- 1 + (mean_cluster_size-1)*icc
+N <- nrow(data)
+eff_N <- N/design
+
+### Compute Bayes factor & posterior probabilities
+bf1 <- BF(x = c(period_x_journal = 
+                  coef(summary(lm_errors))["period:statcheck_journal","Estimate"]), 
+          Sigma = 
+            matrix(coef(summary(lm_errors))["period:statcheck_journal","Std. Error"]^2), 
+          n = eff_N,
+          hypothesis = "period_x_journal < 0")
+bf1
+
+# Posterior probabilities:
+#     Pr(hypothesis|data)
+# H1                   1
+# H2                   0
+# 
+# Evidence matrix (Bayes factors):
+#    H1          H2
+# H1  1 87609295257
+# H2  0           1
+# 
+# Hypotheses:
+#   H1: period_x_journal<0
+#   H2: complement
+
+# ANALYSIS 2: Predict decision inconsistencies ------------------------------------------
+
+# fit multilevel logistic model
+lm_dec_errors <- glmer(decision_error ~ period*statcheck_journal + (1|source), 
+                       data = data, family = binomial(link = "logit"))
+
+summary(lm_dec_errors)
+
+##################
+
+### Compute Bayes factor & posterior probabilities
+bf2 <- BF(x = c(period_x_journal = 
+                  coef(summary(lm_dec_errors))["period:statcheck_journal","Estimate"]), 
+          Sigma = 
+            matrix(coef(summary(lm_dec_errors))["period:statcheck_journal","Std. Error"]^2), 
+          n = eff_N,
+          hypothesis = "period_x_journal < 0")
+bf2
+
+# Posterior probabilities:
+#    Pr(hypothesis|data)
+# H1               0.992
+# H2               0.008
+# 
+# Evidence matrix (Bayes factors):
+#       H1      H2
+# H1 1.000 117.584
+# H2 0.009   1.000
+# 
+# Hypotheses:
+#   H1: period_x_journal<0
+#   H2: complement
--- 04analysis/03exploratory/trends_over_time.R
+++ 04analysis/03exploratory/trends_over_time.R
@@ -2,67 +2,102 @@ rm(list = ls())
 
 # load packages
 library(tidyverse) 
+library(ggpmisc) # for regression equations in plot
 
 # LOAD DATA --------------------------------------------------------------------
 
-# data for all articles, incl those without extracted statistics
-data_all <- read.table("03data/2023-01-06data_wrangled_with_missings.txt", header = TRUE)
-data_per_article_all <- read.table("03data/2023-01-06data_per_article_with_missings.txt", header = TRUE)
-
 # data for articles with statistics
-data <- read.table("03data/2023-01-06data_wrangled_no_missings.txt", header = TRUE)
-data_per_article <- read.table("03data/2023-01-06data_per_article_with_stats.txt", header = TRUE)
+data <- read.table("03data/2023-06-15data_wrangled_no_missings.txt", header = TRUE)
+data_per_article <- read.table("03data/2023-06-15data_per_article_with_stats.txt", header = TRUE)
+
+# reorder factor levels in journal to order panels in plots
+journal_order <- c("PS", "JESP", "JEPG", "JPSP")
+data$journal <- factor(data$journal, levels = journal_order)
+data_per_article$journal <- factor(data_per_article$journal, levels = journal_order)
 
 # FUNCTION FOR PLOT ------------------------------------------------------------
 
 # create general function for the type of plot we're making to save time and
 # avoid errors when changing parameters
 
-make_plot <- function(data, y, color = NULL, perc = TRUE, 
+make_plot <- function(data, y, color = NULL, perc = TRUE, lm = TRUE,
+                      label.y1 = .1, line = TRUE,
+                      ymin = 0, ymax = 100, flag_intervention = TRUE,
                       title = NULL, ylab = NULL){
+  
+  # create data frame with intervention dates per journal to later be able
+  # to add vertical lines at the time of the intervention
+  date_intervention <- 
+    data.frame(journal = factor(journal_order),
+               year_intervention = c(2016 + 7/12, # PS July 2016
+                                     2017 + 8/12, # JESP August 2017
+                                     2016 + 7/12, # PS July 2016
+                                     2017 + 8/12)) # JESP August 2017
+  
   data %>%
     ggplot(aes(x = year_published, y = y, color = color)) +
-    geom_line() +
+    {if (line) geom_line()} +
     geom_point() +
-    {if(perc) scale_y_continuous( labels = function(x) paste0(x, "%"))} +
+    {if(perc) scale_y_continuous(labels = function(x) paste0(x, "%"),
+                                 limits = c(ymin, ymax))} +
+    {if (lm) stat_poly_line()} +
+    {if (lm) stat_poly_eq(use_label(c("eq", "R2")), label.y = label.y1)} +
+    {if (flag_intervention) geom_vline(data = date_intervention, 
+                                       aes(xintercept = year_intervention),
+                                        linetype = 2)} +
+    {if(!is.null(color)) scale_color_discrete(name = "",
+                                              breaks = c("perc_art_error",
+                                                         "perc_art_dec_error"),
+                                              labels = c("Inconsistencies",
+                                                         "Decision\nInconsistencies"))} +
     facet_wrap(~journal) +
     labs(title = title, 
          x = 'Publication Year',
-         y = ylab)
+         y = ylab) +
+    theme_bw()
   
 }
 
 # PERC W NHST OVER TIME --------------------------------------------------------
-
-perc_nhst <- data_per_article_all %>%
-  mutate(has_nhst = ifelse(nr_nhst > 0, 1, 0)) %>%
-  group_by(journal, year_published) %>%
-  summarize(perc_nhst = sum(has_nhst / n())* 100)
-
-make_plot(data = perc_nhst, y = perc_nhst$perc_nhst,
-          title = "% Articles in which statcheck detected NHST results")
+# 
+# perc_nhst_df <- data_per_article_all %>%
+#   mutate(has_nhst = ifelse(nr_nhst > 0, 1, 0)) %>%
+#   group_by(journal, year_published) %>%
+#   summarize(perc_nhst = sum(has_nhst / n())* 100)
+# 
+# make_plot(data = perc_nhst_df, y = perc_nhst_df$perc_nhst,
+#           title = "% Articles in which statcheck detected NHST results") 
 
 # NR NHST OVER TIME ------------------------------------------------------------
 
-mean_nr_nhst <- data_per_article %>%
+mean_nr_nhst_df <- data_per_article %>%
   group_by(journal, year_published) %>%
   summarize(mean_nhst = mean(nr_nhst, na.rm = TRUE))
 
-make_plot(data = mean_nr_nhst, y = mean_nr_nhst$mean_nhst, perc = FALSE,
-          title = "Mean # NHST results in an article for article with NHST results")
+make_plot(data = mean_nr_nhst_df, y = mean_nr_nhst_df$mean_nhst, perc = FALSE,
+          title = "Mean nr. of NHST results in an article for articles with NHST results",
+          label.y1 = .95, ylab = "Nr. of NHST results")
+
+ggsave("04analysis/03exploratory/fig3_nr_nhst_over_time.png", 
+       width = 6.5, height = 4.5, units = "in")
 
 # PERC (DEC) INCONS OVER TIME --------------------------------------------------
-
-perc_errors <- data_per_article %>%
-  group_by(journal, year_published) %>%
-  summarize(mean_errors = mean(perc_errors),
-            mean_dec_errors = mean(perc_dec_errors)) %>%
-  pivot_longer(c(mean_errors, mean_dec_errors), names_to = "type_error", 
-               values_to = "perc")
-
-make_plot(data = perc_errors, y = perc_errors$perc, 
-          color = perc_errors$type_error,
-          title = "Mean % (decision) inconsistencies over time")
+# 
+# perc_errors_df <- data_per_article %>%
+#   group_by(journal, year_published) %>%
+#   summarize(mean_errors = mean(perc_errors),
+#             mean_dec_errors = mean(perc_dec_errors)) %>%
+#   pivot_longer(c(mean_errors, mean_dec_errors), names_to = "type_error",
+#                values_to = "perc")
+# 
+# make_plot(data = perc_errors_df, y = perc_errors_df$perc,
+#           color = perc_errors_df$type_error,
+#           lm = FALSE,
+#           title = "Mean % (decision) inconsistencies over time",
+#           ymin = 0, ymax = 15)
+# 
+# ggsave("04analysis/03exploratory/fig4b_perc_errors_over_time.png", 
+#        width = 6.5, height = 4.5, units = "in")
 
 # PERC ARTICLES W INCONS OVER TIME ---------------------------------------------
 
@@ -73,9 +108,15 @@ articles_w_errors <- data_per_article %>%
   summarize(perc_art_error = sum(has_error)/n()*100,
             perc_art_dec_error = sum(has_dec_error)/n()*100) %>%
   pivot_longer(c(perc_art_error, perc_art_dec_error), names_to = "type_error",
-               values_to = "perc")
+               values_to = "perc") %>%
+  mutate(type_error = factor(type_error, 
+                             levels = c("perc_art_error", "perc_art_dec_error")))
 
 make_plot(data = articles_w_errors, y = articles_w_errors$perc, 
+          lm = FALSE,
           color = articles_w_errors$type_error,
-          title = "% Articles with at least one (decision) inconsistency")
+          title = "% Articles with at least one (decision) inconsistency",
+          ymin = 0, ymax = 85)
 
+ggsave("04analysis/03exploratory/fig4_perc_articles_errors_over_time.png", 
+       width = 6.5, height = 4.5, units = "in")
