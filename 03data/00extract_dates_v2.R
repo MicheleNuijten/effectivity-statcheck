ddee55ca592a9d4570e5ca73f10d440a26ef462a
MicheleNuijtene <michele.nuijten@gmail.com>
2024-04-18 14:47:32 +0200

    rerun scraping script with new date extraction
    --> fixed a bug in date extraction when dates were reported d/m/y instead of m/d/y

---
+++ 03data/00extract_dates_v2.R
@@ -0,0 +1,40 @@
+
+extract_dates <- function(txts){
+  
+  # regex to extract date received/submitted
+  # takes into account format in PS, JEPG, JPSP, and JESP
+  rgx_received <- "(received|submitted).?:?.?\\s*\\d{0,2}\\s*\\w+\\s*\\d{0,2},?\\s*\\d{4}"
+  rgx_date <- "\\d{0,2}\\s*\\w+\\s*\\d{0,2},?\\s*\\d{4}"
+  
+  if (is.null(names(txts))){
+    names(txts) <-  seq_along(txts)
+  }
+  
+  # create empty dataframe to fill with extracted dates
+  date_df <- data.frame(source = names(txts),
+                        date_received = lubridate::as_date(NA))
+  
+  for(i in seq(txts)){
+    
+    # extract (first) full date information
+    raw_date <- extract_pattern(txts[i], rgx_received, ignore.case = TRUE)[1]
+    
+    # only keep actual date
+    if(!is.null(raw_date)){
+      txt_date <- extract_pattern(raw_date, rgx_date, ignore.case = TRUE)
+      
+      # convert to date object taking into account different date formats
+      
+      # if text date starts with digit, assume dmy, otherwise mdy
+      if(grepl("^\\d", txt_date)){
+        date_df$date_received[i] <- lubridate::dmy(txt_date)
+      } else {
+        date_df$date_received[i] <- suppressWarnings(lubridate::mdy(txt_date))
+      }
+    }
+    
+  }
+  
+  return(date_df)
+  
+}
